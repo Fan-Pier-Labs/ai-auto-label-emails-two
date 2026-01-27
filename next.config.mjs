ba3fc21e12c46f863ead68f9f34efa969e798fbb
Ryan Hughes <ryan@fanpierlabs.com>
2026-01-26 20:22:31 -0500

    some fixes for deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
-  experimental: {
-    serverActions: {
-      allowedOrigins: ['localhost:3000'],
-    },
-  },
+  // output: 'standalone',
+  // experimental: {
+  //   serverActions: {
+  //     allowedOrigins: ['localhost:3000'],
+  //   },
+  // },
 }
 
 export default nextConfig
