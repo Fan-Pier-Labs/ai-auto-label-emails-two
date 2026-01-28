23f80a0f2c4300d6d1c1d32ff0c1be150a28e4e9
lkumardudi@gmail.com <lkumardudi@gmail.com>
2026-01-28 16:03:13 +0530

    Set up sentry logs and sessions replays

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -7,4 +7,7 @@ import * as Sentry from "@sentry/nextjs";
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
+  // Sample rate for server-side performance transactions
+  tracesSampleRate: process.env.NODE_ENV === "development" ? 1.0 : 0.1,
+  enableLogs: true,
 });
